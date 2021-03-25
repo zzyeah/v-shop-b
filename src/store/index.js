30a5db02ddfdfaed5a71109b994911f9d615ed01
烨烨 <541767316@qq.com>
2021-03-26 01:51:56 +0800

    登录-数据交互

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { getUserCookie, setUserCookie, removeUserCookie } from '@/utils/userCookie';
 
 Vue.use(Vuex);
 
@@ -7,15 +8,36 @@ export default new Vuex.Store({
   state: {
     // 用于切换菜单的闭合状态 false代表不闭合 true代表闭合
     collapsed: false,
+    // 用户信息
+    user: getUserCookie(),
   },
   mutations: {
     changeCollapsed(state) {
       state.collapsed = !state.collapsed;
     },
+    setUserInfo(state, userInfo) {
+      state.user = userInfo;
+    },
+    logout(state) {
+      state.user = {
+        username: '',
+        appkey: '',
+        role: '',
+        email: '',
+      };
+    },
   },
   actions: {
     changeCollapsed({ commit }) {
       commit('changeCollapsed');
+    },
+    setUserInfo({ commit }, userInfo) {
+      commit('setUserInfo', userInfo);
+      setUserCookie(userInfo);
+    },
+    logout({ commit }) {
+      commit('logout');
+      removeUserCookie();
     },
   },
   modules: {
--- src/views/layout/components/sliderNav.vue
+++ src/views/layout/components/sliderNav.vue
@@ -13,8 +13,8 @@
       </a-breadcrumb>
     </div>
     <ul class="user-info">
-      <li>
-        欢迎帅哥
+      <li class="user-name">
+        {{ $store.state.user.username }}
         <a-icon type="down" />
       </li>
       <li>退出登录</li>
