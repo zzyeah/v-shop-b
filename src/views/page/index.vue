b394def845f91d6d60ff1d529bf9854aea842ba8
烨烨 <541767316@qq.com>
2021-03-27 16:41:37 +0800

    统计页面实现

--- src/views/page/index.vue
+++ src/views/page/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="index">
-    <card></card>
+    <card />
+    <charts />
   </div>
 </template>
 
 <script>
 import Card from '@/components/card.vue';
+import Charts from '@/components/charts.vue';
 
 export default {
   components: {
     Card,
+    Charts,
   },
 };
 </script>
